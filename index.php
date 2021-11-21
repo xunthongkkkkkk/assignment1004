ec28cbba09eab329ae4be3dc1b2b79ba9b52df28
Xun Thong <xunthong.cham@gmail.com>
2021-11-21 18:53:41 +0800

    commit on 'master

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+        <title> Control LED </title>
+</head>
+
+<body>
+<center>
+<h1> Control LED </h1>
+<form method="GET" action="index.php">
+        <button class="red" type="submit" value="ron" name="redL">Turn Red LED On</button>
+        <button class="red" type="submit" value="rblink" name="redL">Blink Red LED</button>
+        <button class="red" type ="submit" value="roff" name="redL">Turn Red LED Off</button>
+
+        <br /><br />
+        <br /><br />
+
+        <button class="green" type="submit" value="gon" name="greenL">Turn Green LED On</button>
+        <button class="green" type="submit" value="gblink" name="greenL">Blink Green LED</button>
+        <button class="green" type="submit" value="goff" name="greenL">Turn Green LED Off</button>
+        <br /><br />
+
+</form>
+</center>
+
+<?php
+        exec("gpio -g mode 0 out");
+        exec("gpio -g mode 0 out");
+
+        if(isset($_GET['redL'])){
+                if($_GET['redL'] == 'ron'){
+                        $argvc = "0 1";
+                        exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+
+                }else if($_GET['redL'] == 'rblink'){
+                        $argvc = "0 2";
+                        exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+
+                }else if($_GET['redL'] == 'roff'){
+                        $argvc = "0 3";
+                        exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+                }
+        }
+
+        if(isset($_GET['greenL'])){
+            if($_GET['greenL'] == 'gon'){
+                    $argvc = "0 4";
+                    exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+            } else if($_GET['greenL'] == 'gblink'){
+                    $argvc = "0 5";
+                    exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+            } else if($_GET['greenL'] == 'goff'){
+                    $argvc = "0 6";
+                    exec("/../../../home/pi/pi/WiringPi/ledBlink/webcontrol $argvc 2>&1");
+            }
+    }
+
+?>
+
+</body>
+
+</html>
--- webcontrol.c
+++ webcontrol.c
@@ -9,67 +9,94 @@
 #define RED_LED 0
 #define GREEN_LED 2
 
+void loops(int);
+
 int main(int argc, char *argv[]){
     //if initialise wiringPi failed
     if(wiringPiSetup() == -1){
-            printf("setup wiringPi failed");
-            exit(1);
+        printf("setup wiringPi failed");
+        exit(1);
     }
-
+    
     int LED_BUTTON = atoi(argv[1]); //return 0 is button is clicked
-    int choice = atoi(argv[2]); //1. On light; 2. Blink; 3. Off light
+    int choice = atoi(argv[2]);
+    //1. On Red light; 2. Blink Red Light; 3. Off Red light
+    //4. On Green Light; 5. Blink Green Light; 6. Blink Green Light
 
     //set led as output mode and button as input mode
     pinMode(RED_LED, OUTPUT);
     pinMode(GREEN_LED, OUTPUT);
 
-    printf("\nPress button to turn on LED.\n");
-
-    //set led initially off light
+    //set led initially on light
     digitalWrite(RED_LED, HIGH);
     digitalWrite(GREEN_LED, HIGH);
 
-    int tries = 0;
-    int blinking = 1;
-
-    for(;;){
-        //if button has been pressed
-        if(LED_BUTTON == 0){
+    //if button has been pressed
+    while(LED_BUTTON == 0){
+        //for red led
+        if(choice >= 1 && choice <= 3){
+            digitalWrite(GREEN_LED, digitalRead(GREEN_LED));
             if(choice == 1){
-                //on led light
+                //on red led light
                 digitalWrite(RED_LED, LOW);
-                digitalWrite(GREEN_LED, LOW);
-                printf("\nLED is on.\n");
-                printf("\nPress button to blink LED.\n");
-                delay(500);
-            }
-            else if(choice == 2){
-                //blink led light
-                blinking = 0;
-                printf("\nLED is blinking.\n");
-                printf("\nPress button to turn off LED.\n");
-                while(blinking == 0){
-                digitalWrite(RED_LED, LOW);
-                digitalWrite(GREEN_LED, LOW);
-                delay(500);
+                LED_BUTTON = 1;
+                
+            } else if(choice == 2){
+                //blink red led light
+                LED_BUTTON = 1;
+                //call loops function such that if 2 or 5 has been passed as argument, 
+                //red or green led will blink
+                loops(2);
+
+            } else{
+                //off red led light
                 digitalWrite(RED_LED, HIGH);
-                digitalWrite(GREEN_LED, HIGH);
-                delay(500);
-                    if(LED_BUTTON == 0){
-                        blinking = 1;
-                        break;
-                    }
-                }
+                LED_BUTTON = 1;
             }
-            else if(choice == 3){
-                //off led light
-                digitalWrite(RED_LED, HIGH);
+        //for green led
+        } else{
+            digitalWrite(RED_LED, digitalRead(RED_LED));
+            if(choice == 4){
+                //on green led light
+                digitalWrite(GREEN_LED, LOW);
+                LED_BUTTON = 1;
+
+            } else if(choice == 5){
+                //blink green led light
+                LED_BUTTON = 1;
+                //call loops function such that if 2 or 5 has been passed as argument, 
+                //red or green led will blink
+                loops(5);
+
+            } else if(choice == 6){
+                //off green led light
                 digitalWrite(GREEN_LED, HIGH);
-                printf("\nLED is off.\n");
-                printf("\nPress button to turn LED on.\n");
-                delay(500);
+                LED_BUTTON = 1;
+                
             }
         }
-    }    
+    }
     return 0;
-}    
+}
+void loops(int choice){
+    for(int i = 0; i < 2; i++){
+        //if choice == 2, blink red led for 2 times
+        if(choice == 2){
+            digitalWrite(RED_LED, LOW);
+            delay(500);
+            digitalWrite(RED_LED, HIGH);
+            delay(500);
+            digitalWrite(RED_LED, LOW);
+            delay(500);
+
+        //if choice == 5, blink green led for 2 times
+        } else if(choice == 5){
+            digitalWrite(GREEN_LED, LOW);
+            delay(500);
+            digitalWrite(GREEN_LED, HIGH);
+            delay(500);
+            digitalWrite(GREEN_LED, LOW);
+            delay(500);
+        }
+    }
+}
